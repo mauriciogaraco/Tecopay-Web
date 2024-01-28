3c8400876ee9bebe15906fa2a8334e8df3eca6ab
Roberto Smith <rsmithvalenz@gmail.com>
2024-01-28 10:00:14 -0800

    Fixing TECOPAY issues 3

--- src/api/APIServices.ts
+++ src/api/APIServices.ts
@@ -3,11 +3,11 @@ import { setKeys } from "../store/slices/sessionSlice";
 
 //Data
 //"https://apidevpay.tecopos.com"
-const baseUrl = `${process.env.REACT_APP_API_HOST_TICKET}`;
+const baseUrl = `${process.env.REACT_APP_API_HOST}`;
 
 //Authentication
 //"https://idapidev.tecopos.com/api"  +   "/v1"
-const baseAuthUrl = `${process.env.REACT_APP_API_HOST}${process.env.REACT_APP_VERSION_API}`;
+const baseAuthUrl = `${process.env.REACT_APP_API_HOST_AUTH}`;
 
 
 //const no_authentication = ["/identity/login"];
